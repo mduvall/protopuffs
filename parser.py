f4e7a76131df7b4376636ac36b326d38df7e06b2
Matthew DuVall <mduvall89@gmail.com>
2011-03-11 17:25:45 -0800

    json encoded

--- parser.py
+++ parser.py
@@ -1,5 +1,5 @@
 # parses the csv file
-
+import json
 global symbol_table, cereals
 symbol_table = { "A": "American Home Food Products",
                  "G": "General Mills",
@@ -16,8 +16,9 @@ def parse():
     with open("cereal.csv", "r") as f:
         for line in f:
             info = line.split(" ")
+            info[0] = info[0].replace("_"," ")
             info[1] = symbol_table[info[1]]
             end = info[len(info)-1]
             info[len(info)-1] = end[0:len(end)-1]
             cereals[info[0]] = info[1:]
-    return cereals
+    return json.dumps(cereals)
