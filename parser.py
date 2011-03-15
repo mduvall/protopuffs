22050d196ae99176fbb807bcd75e1ed808b72e1f
Matthew DuVall <mduvall89@gmail.com>
2011-03-14 19:51:29 -0700

    salt and vitamin visualization done and changed json up a bit

--- parser.py
+++ parser.py
@@ -13,7 +13,7 @@ symbol_table = { "A": "American Home Food Products",
 cereals = []
 carb_fat_cereals = []
 fiber_cereals = {}
-circle_viz = {}
+salt_cereals = []
 def parse():
     count = 0
     with open("static/data/cereal.csv", "r") as f:
@@ -23,34 +23,29 @@ def parse():
             info[1] = symbol_table[info[1]]
             end = info[len(info)-1]
             info[len(info)-1] = end[0:len(end)-1]
-            cereal = {
-                "name": info[0],
-                "manufacturer": info[1],
-                "temperature": info[2],
-                "calories": float(info[3]),
-                "protein": float(info[4]),
-                "fat":float(info[5]),
-                "sodium": info[6],
-                "fiber": float(info[7]),
-                "carbs": float(info[8]),
-                "sugars": info[9],
-                "shelf": info[10],
-                "potassium": info[11],
-                "vitamins": info[12],
-                "weight": info[13],
-                "serving": info[14],
-                "index": count
-             }
             count+=1
             if not (float(info[8]) == -1) and not (float(info[4]) == -1) and not (float(info[5]) == -1):
-                carb_fat_cereals.append(cereal)
+                carb_fat_cereals.append({
+                        "calories":float(info[3]),
+                        "protein":float(info[4]),
+                        "fat":float(info[5]),
+                        "name":info[0],
+                        "carbs":float(info[8])})
             if not (float(info[7]) == -1):
                 try:
                     fiber_cereals[info[1]] = [fiber_cereals[info[1]][0]+float(info[7]),fiber_cereals[info[1]][1]+1]
                 except KeyError:
                     fiber_cereals[info[1]] = [float(info[7]),1]
-            cereals.append(cereal)
+            print info[0], info[6], info[12]
+            if not (float(info[12]) == -1) and not (float(info[6]) == -1):
+                salt_cereals.append({
+                    "name":info[0],
+                    "sodium":float(info[6]),
+                    "vitamins":float(info[12])})
     meaned_fiber_cereals = []
     for manu in fiber_cereals:
-        meaned_fiber_cereals.append({"name":manu, "fiber":(fiber_cereals[manu][0]/fiber_cereals[manu][1])})
-    return [json.dumps(cereals), json.dumps(carb_fat_cereals), json.dumps(meaned_fiber_cereals)]
+        meaned_fiber_cereals.append({
+                "name":manu,
+                "fiber":(fiber_cereals[manu][0]/fiber_cereals[manu][1])})
+    print json.dumps(salt_cereals, indent=4)
+    return [json.dumps(carb_fat_cereals), json.dumps(meaned_fiber_cereals), json.dumps(salt_cereals)]
--- server.py
+++ server.py
@@ -3,8 +3,6 @@ import tornado.web
 import parser
 import os
 
-global cereals
-
 static = {
     "static_path": os.path.join(os.path.dirname(__file__), "static")
     }
@@ -20,17 +18,22 @@ class CalorieHandler(tornado.web.RequestHandler):
 class FiberHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("templates/fiber.html",title="milk&cereal",cereals=fiber_cereals)
-        
+
+class SaltHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("templates/salt.html",title="milk&cereal",cereals=salt_cereals)
+
 application = tornado.web.Application([
         (r"/", MainHandler),
         (r"/calories",CalorieHandler),
         (r"/fiber",FiberHandler),
+        (r"/saltvitamins",SaltHandler),
 ], **static)
 
 if __name__ == "__main__":
     retval = parser.parse()
-    cereals = retval[0]
-    carb_fat_cereals = retval[1]
-    fiber_cereals = retval[2]
+    carb_fat_cereals = retval[0]
+    fiber_cereals = retval[1]
+    salt_cereals = retval[2]
     application.listen(8080)
     tornado.ioloop.IOLoop.instance().start()
